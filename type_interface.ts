d201c171cdcca941d46e9365e794723ca9008458
yonghk423 <yonghk423423@gmail.com>
2022-11-24 13:46:53 +0900

    narrow_type_wide_type

--- type_interface.ts
+++ type_interface.ts
@@ -14,23 +14,23 @@ const yonghee : Human = { bread: true, breath: true, think: true};
 
 // const b: B = { breath: true, bread: true };
 
-interface A { 
-	talk: () => void;
-}
+// interface A { 
+// 	talk: () => void;
+// }
 
-interface A { 
-	eat: () => void;
-}
+// interface A { 
+// 	eat: () => void;
+// }
 
-interface A { 
-	shit: () => void;
-}
+// interface A { 
+// 	shit: () => void;
+// }
 
-const a: A = { talk() {}, eat() {}, shit() {}, sleep: () => {}}
+// const a: A = { talk() {}, eat() {}, shit() {}, sleep: () => {}}
 
-interface A { 
-	sleep: () => void;
-}
+// interface A { 
+// 	sleep: () => void;
+// }
 /*
 확장성 측면
 인터페이스 같은 경우 같은 이름으로 여러번 선언 할 수 가 있고 선언할 때마다 합쳐지게 됩니다. 따라서 협업시에 다른 사람의 인터페이스를 확장 할 수 가 있습니다.  
