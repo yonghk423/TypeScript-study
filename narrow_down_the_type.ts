ab64da3408711c8d479e38aacf49485e485f4d78
yonghk423 <yonghk423423@gmail.com>
2022-12-01 15:45:59 +0900

    type guard

---
+++ narrow_down_the_type.ts
@@ -0,0 +1,9 @@
+function numOrNumArray(a: number | number[]) {
+    if(Array.isArray(a)) {
+        a.concat(4);
+    } else {
+        a.toFixed(3);
+    }
+}
+numOrNumArray(123);
+numOrNumArray([1, 2, 3]);
