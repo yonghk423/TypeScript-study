11e7da60f3d25fbbbd0303cece04575316e700ba
yonghk423 <yonghk423423@gmail.com>
2022-12-15 20:35:07 +0900

    clear

--- narrow_down_the_type.ts
+++ narrow_down_the_type.ts
@@ -1,9 +1,10 @@
 function numOrNumArray(a: number | number[]) {
-    if(Array.isArray(a)) {
-        a.concat(4);
-    } else {
-        a.toFixed(3);
-    }
+  if (Array.isArray(a)) {
+    a.concat(4);
+  } else {
+    a.toFixed(3);
+  }
 }
+
 numOrNumArray(123);
 numOrNumArray([1, 2, 3]);
